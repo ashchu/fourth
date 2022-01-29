c718495dc73640b01ca8be1b2fc19b07ab93129f
Ashley Chu <ashleymchu@yahoo.com>
2022-01-29 07:29:16 -0800

    EARga

--- swehuntlogin.js
+++ swehuntlogin.js
@@ -2,8 +2,8 @@ function validateForm() {
   var username = document.forms["Papaisinthehouse"]["fname"].value;
   var password = document.forms["Papaisinthehouse"]["fpassword"].value;
   if (username.toLowerCase() == "jazzonfleek" && password.toLowerCase() == "kirkland") {
-    alert("The clap of his cheeks rifted space and time");
-    return false;
+    alert("You have passed <3");
+    window.open("https://ashchu.github.io/fifth")
   } else {
     alert("I don't rickroll. Instead I...");
     window.open("https://www.youtube.com/watch?v=y2GYh0bpxd8");
